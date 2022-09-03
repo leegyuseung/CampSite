9b2abb371389b9e569a288670fdead612eeb2a69
leegyuseung <lgs4002@naver.com>
2022-09-03 19:18:44 +0900

    CampSite CRUD 구현해보기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const CampGround = require("./models/campground");
 
 // mongoose & mongoDB
@@ -22,18 +23,58 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// parsing
+app.use(express.urlencoded({ extended: true }));
+// method = put, delete 사용
+app.use(methodOverride("_method"));
+
 // home router
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new CampGround({
-    title: "My Backyard",
-    description: "cheap camping",
+// campgounds/index router
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await CampGround.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// new router
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new CampGround(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// campgrounds/show router
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await CampGround.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+// Update router
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await CampGround.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findByIdAndUpdate(id, {
+    ...req.body.campground,
   });
-  await camp.save();
-  res.send(camp);
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// Delete router
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await CampGround.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
 
 // express 연결
